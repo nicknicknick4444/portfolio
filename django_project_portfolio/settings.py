f13a0e7cca06c3e5a9f1b82b2c3f92472edbe979
unknown <nicknicknick4444@outlook.com>
2022-02-13 18:58:59 +0000

    initial commit

--- django_project_portfolio/settings.py
+++ django_project_portfolio/settings.py
@@ -74,10 +74,6 @@ WSGI_APPLICATION = 'django_project_portfolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -128,3 +124,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
