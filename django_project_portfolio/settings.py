149b377be14b4597ee698c506d466bb6cd88e613
unknown <nicknicknick4444@outlook.com>
2022-02-23 18:25:42 +0000

    low commit

--- django_project_portfolio/settings.py
+++ django_project_portfolio/settings.py
@@ -141,3 +141,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
+
+SESSION_SAVE_EVERY_REQUEST = True
