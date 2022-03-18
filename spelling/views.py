9cb7a48f98924b0a8f52e014cf1edf9a26a85762
unknown <nicknicknick4444@outlook.com>
2022-03-18 20:15:59 +0000

    low commit

--- spelling/views.py
+++ spelling/views.py
@@ -6,7 +6,6 @@ import datetime
 from django.http import HttpResponse, HttpResponseRedirect
 from django.views import generic
 from .models import Tub, Admonish, TweetLists
-from .service import dicty2u
 from .logico.spelling_logic import search_twittor
 from .logico.tweepy_practice import cookie_help, cookie_help_2, getty, send_tweet, api, make_id, get_answers, \
      lookup_answer, readie, make_lists, get_lists, page_turn
