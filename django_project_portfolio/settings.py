b4aea22c74eb8ece5a344945e0d69cba35881d3e
unknown <nicknicknick4444@outlook.com>
2022-03-19 11:43:12 +0000

    low commit

--- django_project_portfolio/settings.py
+++ django_project_portfolio/settings.py
@@ -130,8 +130,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [str(BASE_DIR.joinpath("static"))]
 STATIC_ROOT = str(BASE_DIR.joinpath("staticfiles"))
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-
+#STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
 
 
 # Default primary key field type
