3424cec359b8f6eaee86dd568cc22ec6ee985f6d
unknown <nicknicknick4444@outlook.com>
2022-03-25 19:36:25 +0000

    low commit

--- squaresg/service.py
+++ squaresg/service.py
@@ -1,5 +1,5 @@
 from .logico.squares_logic import make_list, randomise2, make_cou
-from .models import Number, Exceppo, Times
+#from .models import Number, Exceppo, Times
 import random
 
 def make_list_for_view():
@@ -13,12 +13,15 @@ def make_id():
 def initial_cou(listy):
     return make_cou(listy)
 
-def randomise_squares(squares, excep, clicked, SESS):
-    listy, cou = randomise2(squares, excep, clicked, Times, SESS)
-    if cou == "WIN":
-        goes = Number.objects.filter(sessiony=SESS).count()
-    else:
-        goes = 0
+def randomise_squares(squares, excep, clicked, reqc):
+    listy, cou = randomise2(squares, excep, clicked, reqc)
+    #if cou == "WIN":
+        #goes = Number.objects.filter(sessiony=SESS).count()
+    #goes = reqc.COOKIES["goesy"]
+#     else:
+#         goes = 0
     #return randomise2(squares, excep, clicked)
-    return listy, cou, goes
+    return listy, cou
 
+def datechange(the_date, functoken):
+    return functoken(the_date, "%Y-%m-%d %H:%M:%S.%f")
