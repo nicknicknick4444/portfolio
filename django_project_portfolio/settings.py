5c3277d7abfd1136e468e54a67629b170460e607
unknown <nicknicknick4444@outlook.com>
2022-04-02 13:04:12 +0100

    low commit

--- django_project_portfolio/settings.py
+++ django_project_portfolio/settings.py
@@ -26,8 +26,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env.str("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool("DEBUG", default=False)
-#DEBUG = True
+#DEBUG = env.bool("DEBUG", default=False)
+DEBUG = True
 #DEBUG = False
 
 ALLOWED_HOSTS = [".herokuapp.com", "localhost", "127.0.0.1"]
