b74ea4883e7f327aa16135c7121aca03be18aa56
unknown <nicknicknick4444@outlook.com>
2022-04-09 11:17:33 +0100

    low commit

---
+++ plp/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+#from .models import SaveQuery
+
+# Register your models here.
+
+# class SaveQueryAdmin(admin.ModelAdmin):
+#     fieldsets = [
+#         ("session_id", {"fields":["session_id"]}),
+#         ("goodness", {"fields":["goodness"]}),
+#         ("badness", {"fields":["badness"]}),
+#         ]
+#     list_display = ("session_id", "goodness", "badness")
+# 
+# admin.site.register(SaveQuery, SaveQueryAdmin)
