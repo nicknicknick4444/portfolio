e1977615652f7bda09244d23aeb338c20d89b49d
unknown <nicknicknick4444@outlook.com>
2022-04-16 18:27:40 +0100

    low commit

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,22 +1,22 @@
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
-from .models import CustomUser
+#from .models import CustomUser
 
-class CustomUserCreationForm(UserCreationForm):
-    class Meta(UserCreationForm):
-        model = CustomUser
-        fields = (
-            "username",
-            "email",
-            "short_name",
-            )
-    
-class CustomUserChangeForm(UserChangeForm):
-    class Meta:
-        model = CustomUser
-        fields = (
-            "username",
-            "email",
-            "short_name",
-            )
-        
+# class CustomUserCreationForm(UserCreationForm):
+#     class Meta(UserCreationForm):
+#         model = CustomUser
+#         fields = (
+#             "username",
+#             "email",
+#             "short_name",
+#             )
+#     
+# class CustomUserChangeForm(UserChangeForm):
+#     class Meta:
+#         model = CustomUser
+#         fields = (
+#             "username",
+#             "email",
+#             "short_name",
+#             )
+#         
 
