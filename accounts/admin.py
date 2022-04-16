d27478232f5df0242cf33319c4f97e3feb951ff6
unknown <nicknicknick4444@outlook.com>
2022-04-16 18:10:59 +0100

    low commit

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,20 +1,24 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .forms import CustomUserCreationForm, CustomUserChangeForm
-from .models import CustomUser
+#from .forms import CustomUserCreationForm, CustomUserChangeForm
+#from .models import CustomUser
 
 # Register your models here.
 
-class CustomUserAdmin(UserAdmin):
-    add_form = CustomUserCreationForm
-    form = CustomUserChangeForm
-    model = CustomUser
-    list_display = ["email",
-                    "username",
-                    "email",
-                    "short_name",
-                    "is_staff"]
-    fieldsets = UserAdmin.fieldsets + ((None, {"fields": ("short_name",)}),)
-    add_fieldsets = UserAdmin.add_fieldsets + ((None, {"fields": ("short_name",)}),)
-
-admin.site.register(CustomUser, CustomUserAdmin)
+# class CustomUserAdmin(UserAdmin):
+#     #add_form = CustomUserCreationForm
+#     #form = CustomUserChangeForm
+#     #model = CustomUser
+#     model = User
+#     list_display = ["email",
+#                     "username",
+#                     "email",
+#                     #"short_name",
+#                     "is_staff"]
+#     #fieldsets = UserAdmin.fieldsets + ((None, {"fields": ("short_name",)}),)
+#     fieldsets = UserAdmin.fieldsets
+#     #add_fieldsets = UserAdmin.add_fieldsets + ((None, {"fields": ("short_name",)}),)
+#     add_fieldsets = UserAdmin.add_fieldsets
+# 
+# #admin.site.register(CustomUser, CustomUserAdmin)
+# admin.site.register(user, UserAdmin)
--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
-from .forms import CustomUserCreationForm
+#from .forms import CustomUserCreationForm
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 
 class SignUpView(CreateView):
-    form_class = CustomUserCreationForm
+    #form_class = CustomUserCreationForm
+    form_class = UserCreationForm
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
