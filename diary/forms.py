3df80455ca7ff5bc1bedf1372ebb45fb739998e8
unknown <nicknicknick4444@outlook.com>
2022-04-17 16:02:09 +0100

    low commit

--- diary/forms.py
+++ diary/forms.py
@@ -1,5 +1,6 @@
 from django import forms
-from django.forms import Form, ModelForm, DateField
+from django.forms import Form, ModelForm, DateField, ModelMultipleChoiceField
+from django.contrib.auth.models import User
 #from django.forms.extras.widgets import SelectDateWidget
 from .models import Entry
 from functools import partial
@@ -9,19 +10,70 @@ from functools import partial
 #     input_type="date"
 
 
-YEAR_CHOICES = ["2022","2023","2024"]
-MONTH_CHOICES = ["04", "05"]
-DAY_CHOICES = ["01", "02"]
+# YEAR_CHOICES = ["2022","2023","2024"]
+# MONTH_CHOICES = ["04", "05"]
+# DAY_CHOICES = ["01", "02"]
 
-DateInput = partial(forms.DateInput, {"class":"datepicker"})
+class UserChoiceField(ModelMultipleChoiceField):
+    def label_from_instance(self, obj):
+        return obj.first_name
+    
+#DateInput = partial(forms.DateInput, {"class":"datepicker"})
+
+que = User.objects.values_list("first_name", flat=True)
+que2 = User.objects.values_list("first_name", "last_name")
+#que2 = [((i for i in que) i in que) for i in que]
+print([(i,i) for i in que])
+print([i for i in que2])
+#print(que2)
 
 class NewEntryForm(ModelForm):
+#     def __init__(self, *args, **kwargs):
+#         super(NewEntryForm, self).__init__(*args, **kwargs)
+#         self.fields["user"] = forms.ModelMultipleChoiceField(queryset=User.objects.filter(username__startswith="M"))
+    
+    user = forms.ChoiceField(choices=que)
+    #user = UserChoiceField
+    #forms.ChoiceField.input_type="select"
+    #forms.ChoiceField( choices=tuple_of_tuples)
+    #user = forms.CharField(widget=forms.Textarea())
+    
+    #queryset = User.objects.filter(username__startswith="M")
+    #print(queryset)
+#     def get_form(self, request, obj, **kwargs):
+#         form = super(NewEntryForm, self).get_form(request, obj, **kwargs)
+#         form.base_fields["user"] = forms.ModelChoiceField(queryset)
+#         print("PASTE!")
+#         return form
+    
+    #form["user"] = forms.ModelChoiceField(queryset)
+    #print(user)
     forms.DateInput.input_type="date"
+    
+    print("PAPER!", User.objects.filter(first_name__startswith="M"))
+    #print("PAPYRUS!", User.objects.all(), to_field_name="first_name")
+#     def __init__(self, *args, **kwargs):
+#         super(NewEntryForm, self).__init__(*args, **kwargs)
+#         self.fields["user"].initial = self.instance.user.first_name
+#         
+#     def save(self, *args, **kwargs):
+#         super(NewEntryForm, self).save(*args, **kwargs)
+#         self.instance.user.first_name = self.cleaned_data.get("first_name")
+#         self.instance.user.save()
+    
     class Meta:
         model = Entry
-
+        #borce = MyModelChoiceField(queryset=Entry.objects.all())
         fields = "__all__"
         exclude = ("by", "date_written")
+    
+#        def __init__(self, *args, **kwargs):
+#             super(NewEntryForm, self).__init__(*args, **kwargs)
+#             self.fields["user"].queryset = User.objects.all()
+#             self.fields["user"].label_from_instance = lambda obj: obj.first_name
+        
+
+
 
 # class SearchForm(forms.Form):
 #     class Meta:
--- diary/models.py
+++ diary/models.py
@@ -1,6 +1,8 @@
 import datetime
 
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.core.exceptions import ValidationError
@@ -10,25 +12,38 @@ from datetime import date
 
 # Create your models here.
 
-#User._meta.get_field("first_name")._unique = True
+User._meta.get_field("first_name")._unique = True
 
+# class Profile(models.Model):
+#     user = models.OneToOneField(User, on_delete=CASCADE)
 
+#MY = [("a","Yes"),("b","No"),("c","maybe"), ("d","Fat chance")]
+user_query = User.objects.values_list("first_name","last_name")
+USER_CHOICES = [i for i in user_query]
+print(USER_CHOICES)
 
 class Entry(models.Model):
     title = models.CharField(max_length=250)
     detail = models.CharField(max_length=1000)
     #user = models.CharField(max_length=100)
     by = models.CharField(max_length=100, default="")
-    #user = models.OneToOneField(User, to_field="first_name", default="", on_delete=models.CASCADE, unique=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.CharField(max_length=5, choices = USER_CHOICES)
+    #user = models.OneToOneField(User, to_field="first_name", on_delete=models.CASCADE)
+    ###user = models.ForeignKey(User, on_delete=models.CASCADE)
+    #user = models.ModelMultipleChoiceField(queryset=Users.objects.all())
+    #user = models.ForeignKey(User, related_name="first_names", on_delete=models.CASCADE)
     #initial = user.first_name
     ##user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, default=1)
     created = models.DateField()
     last_modified = models.DateField()
     date_for = models.DateField()
     
+#     class Meta:
+#         db_table = "User"
+    
 #     def __str__(self):
-#         return self.user.first_name
+#         return self.user.username
+        #user.get_short_name()
     
     def clean(self, *args, **kwargs):
         super(Entry, self).clean(*args, **kwargs)
@@ -45,6 +60,9 @@ class Entry(models.Model):
     
     def get_absolute_url(self):
         return reverse("diary:home")
+
+# class Initial(models.Model):
+#     models.CharField(max_length=5, unique=True)
     
 class SendTime(models.Model):
     send_time = models.DateTimeField(auto_now_add=True)
