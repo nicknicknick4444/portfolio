a0f76f01cc4b7bb99a2d03ca400ba2f3e3cd2010
unknown <nicknicknick4444@outlook.com>
2022-05-02 15:04:30 +0100

    low commit

--- diary/views.py
+++ diary/views.py
@@ -305,7 +305,7 @@ def search_all(request):
 
 
 def send_emails(request):
-    email_main("Nicky")
+    email_main("Nick")
     template = "diary/done.html"
     return render(request, template, {"today": today})
 
