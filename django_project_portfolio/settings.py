fd0d03337a1416961f42f3e55813259d3344f493
unknown <nicknicknick4444@outlook.com>
2022-06-30 19:41:22 +0100

    Just the migrations and models.py

--- django_project_portfolio/settings.py
+++ django_project_portfolio/settings.py
@@ -27,8 +27,8 @@ SECRET_KEY = env.str("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = env.bool("DEBUG", default=False)
-#DEBUG = True
-DEBUG = False
+DEBUG = True
+#DEBUG = False
 
 ALLOWED_HOSTS = [".herokuapp.com", "localhost", "127.0.0.1"]
 
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
     'spelling.apps.SpellingConfig',
     'plp.apps.PlpConfig',
     'diary.apps.DiaryConfig',
+    'calco.apps.CalcoConfig',
     'accounts.apps.AccountsConfig',
     #3rd Party
     'crispy_forms',
