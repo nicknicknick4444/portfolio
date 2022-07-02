15bed3fce7a0f78f7613c63336953b5a4e2c8102
unknown <nicknicknick4444@outlook.com>
2022-07-02 14:33:34 +0100

    Template Updates

--- calco/urls.py
+++ calco/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path("first/", views.first, name="first"),
     path("colour/", views.colour_pick, name="colour"),
     path("", views.calc1, name="calc1"),
-    path("one/", views.one, name="one"),
+    path("key_process/", views.key_process, name="key_process"),
     path("", views.banty, name="banto")
     ]
