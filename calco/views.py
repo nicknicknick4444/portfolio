15565496231d43668487ba542f43890330f483d5
unknown <nicknicknick4444@outlook.com>
2022-07-02 14:31:29 +0100

    Template Updates

--- calco/views.py
+++ calco/views.py
@@ -27,26 +27,27 @@ def cooko(request, clicked_input):
         
         sum_list = request.COOKIES["sum_list"].split(",")
         
-        print(sum_list)
+        print("CLICKED", type(clicked_input))
         sum_list = [i for i in sum_list[0]]
         sum_list.append(str(clicked_input))
         print("GARCEY!!!!!", sum_list[-1])
-
         print(sum_list, type(sum_list))
         sum_list = "".join(sum_list)
     try:
         if len(sum_list) > 1:
+#             if "ERROR" in sum_list:
+#                 sum_list = sum_list.replace("ERROR","")
             if sum_list[-2] in signs2 and sum_list[-1] in signs2:
                 sum_list = sum_list[:-1]
             elif sum_list[-1] in signs2 and sum_list[-2] == "-" or sum_list[-1] in signs2 and sum_list[-2] == "+":
                 sum_list = sum_list[:-1]
-            elif len(sum_list) >= 4 and sum_list[-3] in signs and sum_list[-2] == "0" and type(eval(sum_list[-1])) == int:
+            elif len(sum_list) >= 4 and sum_list[-3] in signs and sum_list[-2] == "0" and sum_list[-1].isdigit():
                 print("PANS!!!")
                 sum_list = sum_list[:-2] + sum_list[-1]
-            elif len(sum_list) == 2 and sum_list[0] == "0" and type(eval(sum_list[1])) == int:
-                sum_list = sum_list[1]
-    except SyntaxError or NameError:
-        sum_list = "0"
+            elif len(sum_list) == 2 and sum_list[0] == "0" and sum_list[1].isdigit():
+                sum_list = sum_list[1]                
+    except (SyntaxError, NameError) as e:
+        sum_list = "ERROR"
         
             
     return sum_list
@@ -80,12 +81,6 @@ def calc_prep(sum_list):
         to_calc = str(sum_list)
     return to_calc
 
-def no_zero(num):
-    if num[0] == "0":
-        return num[1:]
-    else:
-        return num
-
 def num_disp(sum_list):
     signs = ["+", "-", "*", "/", "(", ")"]
     dispo = []
@@ -97,8 +92,6 @@ def num_disp(sum_list):
             if dispo[-1] in signs:
                 print("PINCERS", "".join(reversed(dispo[:-1])))
                 return "".join(reversed(dispo[:-1]))
-                #return no_zero(num)
-        
         return "".join(reversed(dispo))
     else:
         #return "0"
@@ -119,8 +112,8 @@ def calc1(request):
     template = "calco/calc{}.html".format(calc_num)
 #     buttons = ["CLEAR", "1", "2", "3", "+", "4", "5", "6", "-", "7", "8",\
 #                "9", "x", "0", ".", "÷","(", ")", "="]
-    buttons = ["CLEAR", "1", "2", "3", "+", "4", "5", "6", "-", "7", "8", "9", "x", \
-               "(", ")", ".", "÷", "="]
+    buttons = ["CLEAR", "1", "2", "3", "+", "4", "5", "6", "-", "7", "8", "9", "x", "0", \
+               "(", ")", "÷", ".", "="]
 #     ends = ["+","-","x","÷", "CLEAR"]
     ends = ["CLEAR", "÷"]
     signs = ["+", "-", "*", "/", "(", ")"]
@@ -132,23 +125,56 @@ def calc1(request):
 
         #total = "0"
         #final_list = sum_list
-        if len(final_list) > 0 and final_list[-1] == "=":
+        
+        if len(final_list) > 0 and final_list[-1] == "C":
+            print("TUBES", final_list[-1])
+            final_list = ""
+            total = ""
+            cooky = total
+            response = render(request, template, {"buttons": buttons, "ends": ends, "screen": total, \
+                                                 "colours_list": colours_list, "picked_colour": picked_colour})
+            response.set_cookie("sum_list", cooky)
+            return response
+        elif len(final_list) > 0 and final_list[-1] == "=":
+            print("YAAAAAAARGH!!!!")
             try:
                 total = str(eval(calc_prep(final_list[:-1])))
-            except SyntaxError or NameError:
-                #total = "ERROR"
-                total = "0"
+                print("Miss Burfield")
+            except (SyntaxError, NameError) as e:                    
+                print("CLAGGY")
+                total = "ERROR"
+                #total = "0.0"
+
             if total[-2:] == ".0":
                 total = str(total[:-2])
+#             if total[-1] == "0":
+#                 total = ""
+#         elif len(final_list) >= 3 and final_list[-2:] == "+0":
+#             print("POGGY!")
+#             final_list = ast.literal_eval(final_list[:-2])
+#         elif len(final_list) >= 3 and final_list[-2:] == "-0":
+#             final_list = ast.lieral_evalfinal_list[:-2]
+#             print("KWOGGY")
+        
+        
             cooky = total
             response = render(request, template, {"buttons": buttons, \
                                                   "ends": ends, "screen": total, \
                                                   "colours_list": colours_list, "picked_colour": picked_colour})
             response.set_cookie("sum_list", cooky)
             return response
-        elif len(final_list) > 1 and final_list[-1] == "C" or len(final_list) == 1 and final_list[0] == "C":
+        elif len(final_list) > 1 and final_list[-1] == "C":
             final_list = ""
             total = ""
+        elif len(final_list) == 1 and final_list[0] == "C":
+            final_list = ""
+            total = ""
+#         elif "C" in final_list:
+#             final_list = ""
+#             total = ""
+        elif len(final_list) == 6 and final_list[:-1] == "ERROR":
+            final_list = final_list[-1]
+            total = final_list
             
             print("DIRE STRAITS", total)
             cooky = total
@@ -162,6 +188,9 @@ def calc1(request):
         elif len(final_list) > 0:
             if final_list[-1] in signs:
                 total = final_list[-1]
+#             elif final_list[-1] == "0" and final_list[-2] in signs:
+#                     print("TLAD!")
+#                     total = ""
             # FUNCO HERE
             else:
                 print()
@@ -182,7 +211,7 @@ def calc1(request):
     print("BIA", total)
     return response
 
-def one(request):
+def key_process(request):
     num = request.GET.get("num_id")
     print(num)
     cooky = cooko(request, num)
