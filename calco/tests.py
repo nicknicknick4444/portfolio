f4ed18dddb6f385f2de39cfe502739d390abf653
unknown <nicknicknick4444@outlook.com>
2022-07-10 19:51:13 +0100

    add calc test

--- calco/tests.py
+++ calco/tests.py
@@ -3,7 +3,12 @@ from django.urls import reverse
 from http.cookies import SimpleCookie
 from django.shortcuts import render
 
+from .models import Colour
+
 # Create your tests here.
+
+def create_colour(colour_name, colour_hex):
+    return Colour.objects.create(colour_name=colour_name, colour_hex=colour_hex)
 
 class CalcTest(TestCase):
     def test_main_calc_page_works(self):
@@ -16,3 +21,9 @@ class CalcTest(TestCase):
     def test_first_function_redirects(self):
         response = self.client.post(reverse("calco:first"))
         self.assertEqual(response.status_code, 302)
+    
+    def test_colour_in_db(self):
+        colour_make = create_colour(colour_name="white", colour_hex="#ffffff")
+        colour_set = Colour.objects.all()
+        self.assertQuerysetEqual(colour_set, [colour_make])
+
