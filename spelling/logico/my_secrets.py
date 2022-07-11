2faa6e8944d70d2c652348016356afe6444e2c5f
unknown <nicknicknick4444@outlook.com>
2022-07-11 22:05:30 +0100

    secrets act2

---
+++ spelling/logico/my_secrets.py
@@ -0,0 +1,20 @@
+def BEARER_TOKEN2():
+    return "AAAAAAAAAAAAAAAAAAAAAG%2FIZgEAAAAAA9OgJ56Zkw3BR9s0a1geAvJifJA%3D1ae6zLaCr4noTARaHsRxSnEvEO4QUZ1KxSq7g483wAuaP2D8tI"
+
+def consumer_key2():
+    return "PNeD02vW6jiOxiUR2dTtpt7ml"
+
+def consumer_secret2():
+    return "pVQx2PLw860o4MgF6VrZvFeF7gwIkGQaj4HHqGe1c5Oct2feU0"
+
+def access_token2():
+    return "1498738920242225155-kVRPMEvuiRPCQZOY5hT4DnERrPZojs"
+
+def access_token_secret2():
+    return "nLEOtpGIvamMxML0xe68OwnrxxIA9MsGxLS39UrVKBB2p"
+
+def main():
+    return ""
+
+if __name__ == "__main__":
+    main()
